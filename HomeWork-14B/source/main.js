8df6c02f795a61d039117e3e4d699c63efbaf030
Alexey <alestdnepr@ukr.net>
2025-10-21 15:17:17 +0300

    Update menu in header. Add HomeWork-14B

---
+++ HomeWork-14B/source/main.js
@@ -0,0 +1,24 @@
+document.getElementById("popup").addEventListener("click", function () {
+    document.getElementById("mainform").classList.add("open")
+})
+
+/*
+document.getElementById("popup").addEventListener("click", function () {
+    document.getElementById("loadform").style.display = 'flex'
+})
+*/
+
+document.getElementById("close-mainform").addEventListener("click", function () {
+    document.getElementById("mainform").classList.remove("open")
+})
+
+
+/*
+document.getElementById("main-checkbox").addEventListener("click", function () {
+    document.getElementById("change-field").setAttribute("type", "password")
+})
+
+document.getElementById("main-checkbox").addEventListener("change", function () {
+    document.getElementById("change-field").setAttribute("type", "text")
+})
+*/
